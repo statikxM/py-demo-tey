b73743a2adf65ed8fefa71af84a7a90066c6af27
statikxM <59116568+statikxM@users.noreply.github.com>
2019-12-21 15:59:13 +0200

    next_level

--- program.py
+++ program.py
@@ -1,4 +1,13 @@
 
+# список з 10-ти чисел від 0 до 10
+# усі числa які більше 5, збільшіть на 10
 
-
-print('Hello World!')
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list)
+i = []
+for i in list:
+    if i > 5:
+        i += 10
+        print(i)
+    else:
+        print('This number < 5')
